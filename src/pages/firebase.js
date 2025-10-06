b6ee381193226be6eac38ac2f2e3bc5e3f6cbcc2
Anwar2606 <hfanwar26@gmail.com>
2025-10-06 12:43:35 +0530

    db

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   //testing
-  apiKey: "AIzaSyCTmFMUSQL_lvxZSGzihrx5G7AypB4Uk5Q",
-  authDomain: "testing-855ce.firebaseapp.com",
-  projectId: "testing-855ce",
-  storageBucket: "testing-855ce.appspot.com",
-  messagingSenderId: "1086229411180",
-  appId: "1:1086229411180:web:4a835dadcfb73b08a42f49" 
+  // apiKey: "AIzaSyCTmFMUSQL_lvxZSGzihrx5G7AypB4Uk5Q",
+  // authDomain: "testing-855ce.firebaseapp.com",
+  // projectId: "testing-855ce",
+  // storageBucket: "testing-855ce.appspot.com",
+  // messagingSenderId: "1086229411180",
+  // appId: "1:1086229411180:web:4a835dadcfb73b08a42f49" 
   
     //main
     // apiKey: "AIzaSyD9dhzJrhhENssGVbsGleNDk7ZTBUxsyik",
@@ -26,12 +26,12 @@ const firebaseConfig = {
   // storageBucket: "billing-software2.appspot.com",
   // messagingSenderId: "97163152398",
   // appId: "1:97163152398:web:b02de4e1e085efbca9c05f"
-  // apiKey: "AIzaSyDElnhUdDkx6gADzR9nHYTHopJtd0SwgCQ",
-  // authDomain: "billing1-f7705.firebaseapp.com",
-  // projectId: "billing1-f7705",
-  // storageBucket: "billing1-f7705.firebasestorage.app",
-  // messagingSenderId: "889910896481",
-  // appId: "1:889910896481:web:116615b33b40b24f674720"
+  apiKey: "AIzaSyDElnhUdDkx6gADzR9nHYTHopJtd0SwgCQ",
+  authDomain: "billing1-f7705.firebaseapp.com",
+  projectId: "billing1-f7705",
+  storageBucket: "billing1-f7705.firebasestorage.app",
+  messagingSenderId: "889910896481",
+  appId: "1:889910896481:web:116615b33b40b24f674720"
 };
 
 const app = initializeApp(firebaseConfig);
